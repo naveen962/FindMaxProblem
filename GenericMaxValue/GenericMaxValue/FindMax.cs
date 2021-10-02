9e89ef7fe129e59736e9b04de34dfee2cfe7f52a
naveen962 <mrn28897@gmail.com>
2021-10-02 04:53:08 -0700

    program to find maximum of 3 number by using generic method is added

---
+++ GenericMaxValue/GenericMaxValue/FindMax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMaxValue
+{
+   public class FindMax<T>where T:IComparable
+    {
+      public static T[] Max(T []inputarray)
+      {
+            Array.Sort(inputarray);
+          
+            return inputarray;
+      }
+
+    
+
+
+   }
+}
+           
+    
+
---
+++ GenericMaxValue/GenericMaxValue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] intarray = { 10, 30, 20 };
+            double[] doublearray = { 22.2, 53.3, 44.4 };
+            string[] stringarray = { "Naveen", "Raj", "Abhishek" };
+
+           int[] res1array= FindMax<int>.Max(intarray);
+            int index1= res1array.Length - 1;
+            Console.WriteLine(res1array[index1]);
+           double[]res2array= FindMax<double>.Max(doublearray);
+            int index2 = res1array.Length - 1;
+            Console.WriteLine(res2array[index2]);
+            string[]res3array= FindMax<string>.Max(stringarray);
+            int index3 = res1array.Length - 1;
+            Console.WriteLine(res3array[index3]);
+        }
+    }
+}

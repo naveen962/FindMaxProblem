7597c4ff4a355543360b3e3ec1a058b2ea9dbd66
naveen962 <mrn28897@gmail.com>
2021-10-02 04:57:48 -0700

    program to find the maximum of 3 numbers using generic class is added

---
+++ GenclassMaxValue/GenclassMaxValue/MaxValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenclassMaxValue
+{
+   public class MaxValue<T> where T: IComparable
+    {
+        public T Take_input1, Take_input2, Take_input3; // declare veriables
+        public MaxValue(T Take_input1, T Take_input2, T Take_input3) //Constructor
+        {
+            this.Take_input1 = Take_input1;
+            this.Take_input2 = Take_input2;
+            this.Take_input3 = Take_input3;
+        }
+        public  T getMax()
+        {
+            var MaxResult = this.Take_input1.CompareTo(Take_input2) > 0 ? Take_input1 : (this.Take_input2.CompareTo(Take_input3) > 0 ? Take_input2 : Take_input3); //Find maximum number/result
+
+            Console.WriteLine("Maximum value is : " + MaxResult);
+
+            return MaxResult;
+        }
+    }
+}
---
+++ GenclassMaxValue/GenclassMaxValue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenclassMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxValue<int> mv1 = new MaxValue<int>(10, 20, 30);
+            int resarray1=mv1.getMax();
+            Console.WriteLine(resarray1);
+
+            MaxValue<double> mv2 = new MaxValue<double>(10.3, 20.5, 3.0);
+            double resarray2 = mv2.getMax();
+            Console.WriteLine(resarray2);
+
+            MaxValue<string> mv3 = new MaxValue<string>("Naveen","Raj","Abhi");
+            string resarray3 = mv3.getMax();
+            Console.WriteLine(resarray3);
+        }
+    }
+}

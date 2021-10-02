7826fed7fe5a7f18bb1b7c463b964d32afc61c99
naveen962 <mrn28897@gmail.com>
2021-10-02 08:20:51 -0700

    program to find the maximum of 3 numbers using generic method is added

--- GenericMaxValue/GenericMaxValue/FindMax.cs
+++ GenericMaxValue/GenericMaxValue/FindMax.cs
@@ -4,14 +4,26 @@ using System.Text;
 
 namespace GenericMaxValue
 {
-   public class FindMax<T>where T:IComparable
+   public class FindMax
     {
-      public static T[] Max(T []inputarray)
+      public static T Max<T>(T first,T second,T Third)where T:IComparable
       {
-            Array.Sort(inputarray);
-          
-            return inputarray;
-      }
+            if (first.CompareTo(second)>0 && first.CompareTo(Third)>0)
+            {
+                return first;
+            }
+
+           else if (second.CompareTo(first) > 0 && second.CompareTo(Third) > 0)
+            {
+                return second;
+            }
+            else
+            {
+                return Third;
+            }
+
+
+        }
 
     
 
--- GenericMaxValue/GenericMaxValue/Program.cs
+++ GenericMaxValue/GenericMaxValue/Program.cs
@@ -6,20 +6,13 @@ namespace GenericMaxValue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] intarray = { 10, 30, 20 };
-            double[] doublearray = { 22.2, 53.3, 44.4 };
-            string[] stringarray = { "Naveen", "Raj", "Abhishek" };
+           var res1= FindMax.Max<int>(10, 20, 30);
+            Console.WriteLine(res1);
+          var res2 = FindMax.Max<double>(10.3, 2.0, 3.0);
+            Console.WriteLine(res2);
+            var res3 = FindMax.Max<string>("Raj", "abay", "sujesh");
+            Console.WriteLine(res3);
 
-           int[] res1array= FindMax<int>.Max(intarray);
-            int index1= res1array.Length - 1;
-            Console.WriteLine(res1array[index1]);
-           double[]res2array= FindMax<double>.Max(doublearray);
-            int index2 = res1array.Length - 1;
-            Console.WriteLine(res2array[index2]);
-            string[]res3array= FindMax<string>.Max(stringarray);
-            int index3 = res1array.Length - 1;
-            Console.WriteLine(res3array[index3]);
         }
     }
 }

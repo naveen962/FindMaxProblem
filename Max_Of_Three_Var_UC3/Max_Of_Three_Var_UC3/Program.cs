28715b61204c78e9b3c5d26e39deca94ebe76696
naveen962 <mrn28897@gmail.com>
2021-10-02 04:48:46 -0700

    program to find the maximum of 3 strings is added

---
+++ Max_Of_Three_Var_UC3/Max_Of_Three_Var_UC3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Max_Of_Three_Var_UC3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----Testcase1-----");
+            string res1=Testcase1.Max1("Peach", "Apple", "Banana");
+            Console.WriteLine(res1);
+            Console.WriteLine("-----Testcase2-----");
+            string res2=Testcase2.Max2("Peach", "Strawberry", "Banana");
+            Console.WriteLine(res2);
+            Console.WriteLine("-----Testcase3-----");
+            string res3=Testcase3.Max3("Banana", "Peach", "Tomato");
+            Console.WriteLine(res3);
+
+        }
+    }
+}
---
+++ Max_Of_Three_Var_UC3/Max_Of_Three_Var_UC3/Testcase2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Max_Of_Three_Var_UC3
+{
+    class Testcase2
+    {
+        public static string Max2(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+
+            }
+            else
+            {
+                return third;
+
+            }
+        }
+    }
+}

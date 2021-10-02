a46ed4de3d9c33a9a2a54bdc577c879be6c9d60a
naveen962 <mrn28897@gmail.com>
2021-10-02 05:05:02 -0700

    program to find the maximum of numbers having more than 3 parameters using generic method is added

---
+++ Max_Of_Three_Var_UC5/Max_Of_Three_Var_UC5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Max_Of_Three_Var_UC5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intarray = { 10, 33, 43, 65, 22 };
+            double[] doublearray = { 2.2, 6.4, 7.3, 4.2, 7.7 };
+            string[] stringarray = { "naveen", "raj", "abhishek", "sujesh", "kamalesh" };
+            SortMaxValue<int> mv1 = new SortMaxValue<int>(intarray);
+            mv1.PrintMax();
+            
+            SortMaxValue<double> mv2 = new SortMaxValue<double>(doublearray);
+            mv2.PrintMax();
+            
+            SortMaxValue<string> mv3 = new SortMaxValue<string>(stringarray);
+            mv3.PrintMax();
+           
+
+
+        }
+    }
+}
+
+
---
+++ Max_Of_Three_Var_UC5/Max_Of_Three_Var_UC5/SortMaxValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Max_Of_Three_Var_UC5
+{
+   public class SortMaxValue<T>where T:IComparable
+    {
+        private T[] inputarray;
+        public  SortMaxValue(T[] inputarray)
+        {
+            this.inputarray = inputarray;
+           
+        }
+        public void PrintMax()
+        {
+            Array.Sort(inputarray);
+            var maximum = this.inputarray[this.inputarray.Length - 1];
+
+            GetSort(maximum);
+            
+        }
+
+        public void GetSort(T Maximum)
+        {
+
+           
+            Console.WriteLine("The Maximum value is: "+Maximum);
+
+        }
+    }
+}
